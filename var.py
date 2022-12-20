0cbda9b54a4c94bca19bdab7f9e5728d375c3554
RiZoeL -! <85222112+MrRizoel@users.noreply.github.com>
2022-12-20 11:18:53 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,9 +1,21 @@
-from decouple import config
-from heroku3 import from_key
-from os import getenv
+import os
+from dotenv import load_dotenv
+
+if os.path.exists(".env"):
+    load_dotenv(".env")
+
+def make_int(str_input):
+    str_list = str_input.split(" ")
+    int_list = []
+    for x in str_list:
+        int_list.append(int(x))
+    return int_list
 
 class Var:
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", None)
-    BOT_TOKEN = config("BOT_TOKEN", None)
-    SUDO = list(map(int, getenv("SUDO").split()))
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    sudo = os.getenv("SUDO")
+    SUDO = []
+    if sudo:
+        SUDO = make_int(sudo)
